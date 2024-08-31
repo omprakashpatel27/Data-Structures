82c0578b124ea69227691a72c4a5339b754c096b
omprakashpatel27 <omprakashpatel62@gmail.com>
2024-08-31 15:17:54 +0530

    added game thorey problem & bezout theorem

---
+++ bezout_theorem.cpp
@@ -0,0 +1,13 @@
+/*
+
+for the equation xa + yb , where x & y are coefficents , we can choose any value for x & y , it always gives some multiple of gcd of a & b
+e.g if a & b are 15 & 21 , there gcd is 3 , using any value x & y this equation 15x + 21y will gives some multiple of 3 
+
+2 * 15 + 3 * 21 => 93 % 3 => 0
+
+
+Problem : https://codeforces.com/contest/2007/problem/C
+Solution : https://codeforces.com/contest/2007/submission/278853574
+
+
+*/
--- game_theory.cpp
+++ game_theory.cpp
@@ -5,4 +5,7 @@ Explanation is there in comments of solution
 Problem : https://codeforces.com/contest/2004/problem/E
 Solution : https://codeforces.com/contest/2004/submission/276830458
 
+Problem : https://atcoder.jp/contests/abc368/tasks/abc368_f
+
 */
+
